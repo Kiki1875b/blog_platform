31397e0d2bc5a73b9d87ef843456ffd631ab8131
Kiki1875b <kevinheo0413@gmail.com>
2025-09-09 15:49:25 +0900

    feat: MemberService 에 사용자  ID 조회 추가

--- src/main/java/com/example/blog/domain/member/service/MemberService.java
+++ src/main/java/com/example/blog/domain/member/service/MemberService.java
@@ -4,8 +4,10 @@ package com.example.blog.domain.member.service;
 import com.example.blog.auth.user_details.CustomPrincipal;
 import com.example.blog.domain.member.dto.UpdateMemberRequestDto;
 import com.example.blog.domain.member.entity.Member;
+import java.util.UUID;
 
 public interface MemberService {
   Member findMember(CustomPrincipal principal);
   Member updateMember(UpdateMemberRequestDto request, CustomPrincipal principal);
+  Member findMemberById(UUID memberId);
 }
