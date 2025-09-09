15def6e9801e9792940348a474de7cec9e66e030
Kiki1875b <kevinheo0413@gmail.com>
2025-09-09 13:07:15 +0900

    feat: blog 페이지네이션 구현

--- src/main/java/com/example/blog/domain/blog/controller/BlogController.java
+++ src/main/java/com/example/blog/domain/blog/controller/BlogController.java
@@ -1,34 +1,49 @@
 package com.example.blog.domain.blog.controller;
 
 
-
 import com.example.blog.auth.user_details.CustomPrincipal;
+import com.example.blog.common.pagenation.PaginatedResponse;
+import com.example.blog.domain.blog.dto.BlogPaginationRequest;
 import com.example.blog.domain.blog.dto.BlogResponseDto;
 import com.example.blog.domain.blog.dto.CreateBlogRequestDto;
-import com.example.blog.domain.blog.service.BlogService;
+import com.example.blog.domain.blog.facade.BlogFacade;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/blogs")
+@RequestMapping("/api")
 @RequiredArgsConstructor
 public class BlogController {
 
 
-  private final BlogService blogService;
+  private final BlogFacade blogFacade;
 
-  @PostMapping
+  @PostMapping("/blogs")
   public ResponseEntity<BlogResponseDto> createBlog(@RequestBody CreateBlogRequestDto blogRequest, @AuthenticationPrincipal
       CustomPrincipal principal){
 
-    BlogResponseDto responseDto = blogService.createBlog(blogRequest, principal);
+    BlogResponseDto responseDto = blogFacade.createBlog(blogRequest, principal);
 
     return ResponseEntity.ok(responseDto);
+  }
+
+  @GetMapping("/member/{memberId}/blogs")
+  public ResponseEntity<PaginatedResponse<BlogResponseDto>> getMemberBlogs(
+      @PathVariable UUID memberId,
+      @ModelAttribute BlogPaginationRequest blogRequest
+  ){
+
+    PaginatedResponse<BlogResponseDto> res = blogFacade.getMemberBlogs(memberId, blogRequest);
+
+    return ResponseEntity.ok(res);
   }
 }
